dd914db382c75f57384a8d17e3c3ea3b77d2945a
Thomas Marchand <me@thomas-marchand.com>
2021-07-22 21:27:35 +0200

    initial commit

---
+++ oraxen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import argparse
+import json
+
+parser = argparse.ArgumentParser("oraxen.py")
+parser.add_argument("model", help="path to a model file", type=str)
+parser.add_argument("--output", help="path to the output model file", type=str)
+args = parser.parse_args()
+
+with open(args.model) as model_file:
+    content = json.load(model_file)
+    optimized_output = json.dumps(content).replace(" ", "")
+    with open(args.model.replace(".json", "") + "_optimized.json", 'w') as output_file:
+        output_file.write(optimized_output)
